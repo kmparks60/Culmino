4e1853a167f33971e6073b9c296e11565f7b2968
Mason Parks <kmparks60@gmail.com>
2024-09-19 11:10:08 -0400

    Updated requirements.txt, .env, and run.py files.

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
 from application import app
+
+load_dotenv()
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	port = int(os.environ.get('PORT'))
+	app.run(host='0.0.0.0', port=port)
